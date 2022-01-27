3a620715a95f8c9ebba6536f40d5e7bbbf6ee1f8
hoangtv-2888 <tran.viet.hoang@sun-asterisk.com>
2022-01-27 09:02:39 +0700

    fix product_detail

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   before_action :load_product, only: :show
+
   def show; end
 
   private
@@ -7,16 +8,16 @@ class ProductsController < ApplicationController
   def load_product
     @product = Product.includes(:product_detail).find_by id: params[:id]
 
-    if @product&.product_detail
-      @product_color = []
-      @product_size = []
-      @product.product_detail.each do |product_detail|
-        @product_color << product_detail.product_color
-        @product_size << product_detail.product_size
-      end
-      return
+    unless @product
+      flash[:danger] = t "not_found"
+      return redirect_to root_path
     end
-    flash[:danger] = t "not_found"
-    redirect_to root_path
+
+    @product_color = []
+    @product_size = []
+    @product.product_detail.each do |product_detail|
+      @product_color << product_detail.product_color
+      @product_size << product_detail.product_size
+    end
   end
 end
