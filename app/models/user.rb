89f076d7188946d9678a7101bdfebbd18fa5fa18
buixuantuong <bui.xuan.tuong@sun-asterisk.com>
2022-02-23 23:06:44 +0700

    gem devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   PROPERTIES = %i(name email address phone password
                   password_confirmation remember_me).freeze
   devise :database_authenticatable, :registerable, :validatable,
-         :confirmable, :recoverable, :rememberable
+         :confirmable, :recoverable, :rememberable,
+         :trackable, :timeoutable, :lockable
   has_many :orders, dependent: :destroy
   has_many :comment_rates, dependent: :destroy
   has_many :products, through: :comment_rates
@@ -14,17 +15,17 @@ class User < ApplicationRecord
 
   validates :name, presence: true,
             length: {maximum: Settings.length_digit_255}
-  validates :email, presence: true,
-            length: {maximum: Settings.length_digit_255},
-            format: {with: Settings.email_regex}
-  validates :password, presence: true,
-            length: {minimum: Settings.length_digit_6},
-            allow_nil: true
   scope :search_by_name,
         ->(name){where("LOWER(name) LIKE ?", "%#{name.downcase}%") if name}
 
   private
   def downcase_email
     email.downcase!
+  end
+
+  def timeout_in
+    return 10.seconds if admin?
+
+    10.seconds
   end
 end
